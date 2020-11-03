fc951dcc133a0816ecc94588fb3ad002135a556a
Stevan Stanisic <stevan.stanisic@outlook.com>
2020-11-03 16:11:29 +0100

    Co-authored-by: Nenad Zivotic <nenadzivotic94@gmail.com>

---
+++ frontend/src/store.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers, compose, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import Cookie from "js-cookie"
+import { productDetailsReducer, productListReducer } from "./reducers/productReducers"
+import { cartReducer } from "./reducers/cartReducers"
+import { userSigninReducer } from "./reducers/userReducers"
+
+const cartItems = Cookie.getJSON("cartItems") || []
+const userInfo = Cookie.getJSON("userInfo") || null
+
+const initialState = { cart: { cartItems }, userSignin: { userInfo } }
+
+const reducer = combineReducers({
+	productList: productListReducer,
+	productDetails: productDetailsReducer,
+	cart: cartReducer,
+	userSignin: userSigninReducer,
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)))
+
+export default store
